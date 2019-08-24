489e80e84bcc3f45d954fcc951841322c18f0a51
ty8900 <ty8900@naver.com>
2019-08-24 17:40:23 +0900

    8/24 DS
    *골목
    배경 구현/카메라 이동 구현/양옆 이동불가 구현/캐릭터 시선 구현

---
+++ JJYqt/Assets/Scripts/CameraMove.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public GameObject player;
+
+    private Vector3 temp;
+    private float min1 = -6.6f; //왼쪽 배경 최댓값
+    private float max1 = 5.4f;  //오른쪽 배경 최댓값
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.position = new Vector3(min1, -5.06f, -10f); // 초기 카메라 위치
+        temp = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    { 
+         temp = transform.position;
+         float dif = transform.position.x - player.transform.position.x;
+         if(dif < -4.6f && transform.position.x != max1) //카메라와 플레이어 위치 간격이 -4.6보다 작으면 오른쪽으로 카메라 이동
+         {
+            temp.x += (-4.6f - dif);
+         }
+         if (dif > 6f)  // 카메라와 플레이어 위치 간격이 6보다 크면 왼쪽으로 카메라 이동 / 수치는 변경 가능 및 임의로 설정한 것 보기 좋은 각도로
+        {
+            {
+            temp.x -= (dif - 6f);
+         }
+            
+         if (temp.x < min1)
+         {
+             temp.x = min1;
+         }
+         if (temp.x > max1)
+         {
+             temp.x = max1;
+         }
+    }
+
+    private void FixedUpdate()
+    {
+        transform.position = temp;
+    }
+}
--- JJYqt/Assets/Scripts/PlayerMove.cs
+++ JJYqt/Assets/Scripts/PlayerMove.cs
@@ -8,13 +8,13 @@ public class PlayerMove : MonoBehaviour
     public float moveforce;
 
     private Transform site;
-    private bool isJump,isGround;
-    private float t;
+    private bool isJump,isGround,rot;//점프가능/땅에닿은 여부/왼쪽오른쪽 바라보는 방향
     // Start is called before the first frame update
     void Start()
     {
         site = transform;
         isGround = true;
+        rot = false;
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -26,14 +26,15 @@ public class PlayerMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        t += Time.deltaTime;
         if (Input.GetKey(KeyCode.LeftArrow))
         {
             site.Translate(Vector3.left * movespeed * Time.deltaTime, Space.World);
+            rot = false;
         }
         if (Input.GetKey(KeyCode.RightArrow))
         {
             site.Translate(Vector3.right * movespeed * Time.deltaTime, Space.World);
+            rot = true;
         }
         if (Input.GetButtonDown("Jump") && isGround)
         {
@@ -43,6 +44,10 @@ public class PlayerMove : MonoBehaviour
     }
     void FixedUpdate()
     {
+        if(rot)
+        transform.rotation = Quaternion.Euler(0, 0, 0);
+        else
+        transform.rotation = Quaternion.Euler(0, 180, 0);
         Jump();
     }
 
