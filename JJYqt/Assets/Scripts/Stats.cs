0cd0019dd07beb292c2b25ca4d1c5764a6ea69f1
ty8900 <ty8900@naver.com>
2019-08-26 17:05:36 +0900

    8/26 DS
    사소한 변경(캐릭터 회전 부드럽게,
    주인공/몹 상속할 스탯 스크립트)

---
+++ JJYqt/Assets/Scripts/Stats.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour
+{
+    private float HP;
+    private float MP;
+    private float 
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
