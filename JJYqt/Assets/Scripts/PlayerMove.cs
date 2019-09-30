44fb6b471aba2c4c063943adf569bda48019bdec
creeper00 <rnjswldnd@snu.ac.kr>
2019-09-30 11:26:07 +0900

    ui added

--- JJYqt/Assets/Scripts/PlayerMove.cs
+++ JJYqt/Assets/Scripts/PlayerMove.cs
@@ -11,7 +11,6 @@ public class PlayerMove : MonoBehaviour
     public float moveforce;
     public float posx, posy, posz;
     public int HPcon; // HP control
-    Color damageColor;
     public Material damagedMaterial;
     public GameObject Weapon;
     public int Attackcon=0; // Attack control attack 도중 또 attack 불가
@@ -28,7 +27,6 @@ public class PlayerMove : MonoBehaviour
         isGround = true;
         rot = false;
         HPcon = 1;
-        damageColor = new Color(111f, 111f, 111f);
     }
 
     private void OnCollisionEnter2D(Collision2D collision)
@@ -37,54 +35,16 @@ public class PlayerMove : MonoBehaviour
         Debug.Log(collision.gameObject.tag);
         if (collision.gameObject == Wall)
             isGround = true;
-        if(collision.gameObject.tag == "enemy" && HPcon==1 )
+        if (collision.gameObject.tag == "enemy" && HPcon == 1)
         {
             posx = transform.position.x;
             posy = transform.position.y;
             posz = transform.position.z;
             Stats.instance.minusHP();
-            if(collision.gameObject.transform.position.x > posx) transform.position = new Vector3(posx-150f, posy, posz);
+            if (collision.gameObject.transform.position.x > posx) transform.position = new Vector3(posx - 150f, posy, posz);
             else transform.position = new Vector3(posx + 150f, posy, posz);
-            StartCoroutine("waitPlayer");
+            StartCoroutine("whenDamaged");
         }
-    }
-    IEnumerator waitPlayer()
-    {
-        float save = movespeed;
-        movespeed = 0;
-        HPcon = 0;
-        SpriteRenderer rend = GetComponent<SpriteRenderer>();
-        SpriteRenderer rend2 = Weapon.GetComponent<SpriteRenderer>();
-        Material original = rend.material;
-        for(int i=10; i>0; i--)
-        {
-            if (i % 2 == 0) { rend.material = damagedMaterial; rend2.material = damagedMaterial; }
-            else { rend.material = original; rend2.material = original; }
-            yield return new WaitForSeconds(0.05f);
-        }
-        movespeed = save;
-        HPcon = 1;
-    }
-
-    IEnumerator Attack()
-    {
-        Attackcon = 1;
-        float rox = Weapon.transform.eulerAngles.x;
-        float roy = Weapon.transform.eulerAngles.y;
-        float roz = Weapon.transform.eulerAngles.z;
-        Quaternion q=Weapon.transform.rotation;
-        for (int i=10; i>0; i--)
-        {
-            if (rot) roy = 0;
-            else roy = 180;
-            Debug.Log(i+" "+roz);
-            if (i > 5) roz -= 10;
-            else  roz += 10;
-            q = Quaternion.Euler(rox, roy, roz);
-            Weapon.transform.rotation = q;
-            yield return new WaitForSeconds(0.02f);
-        }
-        Attackcon = 0;
     }
 
     // Update is called once per frame
@@ -127,5 +87,44 @@ public class PlayerMove : MonoBehaviour
             gameObject.GetComponent<Rigidbody2D>().AddForce(Vector3.up * moveforce);
             isJump = false;
         }
+    }
+
+    IEnumerator whenDamaged() 
+    {
+        float save = movespeed;
+        movespeed = 0;
+        HPcon = 0;
+        SpriteRenderer rend = GetComponent<SpriteRenderer>();
+        SpriteRenderer rend2 = Weapon.GetComponent<SpriteRenderer>();
+        Material original = rend.material;
+        for (int i = 10; i > 0; i--)
+        {
+            if (i % 2 == 0) { rend.material = damagedMaterial; rend2.material = damagedMaterial; }
+            else { rend.material = original; rend2.material = original; }
+            yield return new WaitForSeconds(0.05f);
+        }
+        movespeed = save;
+        HPcon = 1;
+    }
+
+    IEnumerator Attack()
+    {
+        Attackcon = 1;
+        float rox = Weapon.transform.eulerAngles.x;
+        float roy = Weapon.transform.eulerAngles.y;
+        float roz = Weapon.transform.eulerAngles.z;
+        Quaternion q = Weapon.transform.rotation;
+        for (int i = 10; i > 0; i--)
+        {
+            if (rot) roy = 0;
+            else roy = 180;
+            Debug.Log(i + " " + roz);
+            if (i > 5) roz -= 10;
+            else roz += 10;
+            q = Quaternion.Euler(rox, roy, roz);
+            Weapon.transform.rotation = q;
+            yield return new WaitForSeconds(0.02f);
+        }
+        Attackcon = 0;
     }
 }
